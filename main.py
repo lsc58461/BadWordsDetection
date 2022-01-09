e767a1d0657bcd5c2fc182e7f118c57a677828d3
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2022-01-09 16:01:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,32 +20,36 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    word = str(message.content)
-    a.input=word
-    a.text_modification()
-    a.lime_compare(a.token_badwords , a.token_detach_text[0], 0.9)
-    result = a.result
-    a.lime_compare(a.new_token_badwords , a.token_detach_text[1], 0.9 , True)
-    result += a.result
-    if len(result) == 0:
-        return None
-    for j in result:
+    if message.author == client.user:
+        return
+    else:
+        word = str(message.content)
+        a.input=word
+        a.text_modification()
+        a.lime_compare(a.token_badwords , a.token_detach_text[0], 0.9)
+        result = a.result
+        a.lime_compare(a.new_token_badwords , a.token_detach_text[1], 0.9 , True)
+        result += a.result        
+        if len(result) == 0:
+            return None
+        for j in result:
             word = word[0:j[0]] + '-'*(j[1]-j[0]+1) + word[j[1]+1:]
-    embed = discord.Embed(title = '' , color = 0xFF3636)
-    embed.add_field(name='----------    필터링된 채팅    ----------', value = word)
-    embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
-    MyEmbed = Embed(
-        title = "비속어 감지",
-        color = 0xFF4848
-    ).add_field(
-        name = "────────────────────────",
-        value = f"{str(message.author)}님이 [{word}]에서 비속어 [{result}]을(를) 사용하셨습니다.\n────────────────────────",
-        inline = True
-    )
-    channel = client.get_channel(927067418017292339)
-    await channel.send(embed=MyEmbed)
-    await message.channel.send('어머')
-    await message.channel.send(embed = embed)
-    await message.delete()
+            MyEmbed = discord.Embed(
+                title = "비속어 감지",
+                color = 0xFF4848
+            ).add_field(
+                name = "────────────────────────",
+                value = f"{str(message.author)}님이 사용한 [{str(message.content)}]에서 비속어 {result}이가 감지 되었습니다.\n────────────────────────",
+                inline = True
+            )
+            channel = client.get_channel(927067418017292339)
+            await channel.send(embed=MyEmbed)
+        embed = discord.Embed(title = '' , color = 0xFF3636)
+        embed.add_field(name='----------    필터링된 채팅    ----------', value = word)
+        embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+        
+        await message.channel.send('어머')
+        await message.channel.send(embed = embed)
+        await message.delete()
         
 client.run(Token)
